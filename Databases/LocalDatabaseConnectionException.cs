e9dfbd90f8d22bea3333e88cc1e41067a3ada8c3
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-24 17:35:01 +0200

    added custom exception for troubles with retrieving data from local database

---
+++ Databases/LocalDatabaseConnectionException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databases
+{
+    class LocalDatabaseConnectionException : Exception
+    {
+        public LocalDatabaseConnectionException()
+        {
+
+        }
+
+        public LocalDatabaseConnectionException(string msg)
+            : base(String.Format("The exception message text: {0}", msg))
+        {
+
+        }
+    }
+}
