10f0ce172132d12c575a444a888a93b75dd02ccd
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-24 15:25:15 +0200

    added class for local dbs config information

--- Databases/LocalDBConfigInfo.cs
+++ Databases/LocalDBConfigInfo.cs
@@ -8,6 +8,16 @@ namespace Databases
 {
     class LocalDBConfigInfo
     {
+        public string DataSource { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
+        public string TableName { get; set; }
 
+        public string GetConnectionString()
+        {
+            return String.Format("datasource={0};port={1};username={2};password={3};database={4};", DataSource, Port, UserName, Password, DatabaseName);
+        }
     }
 }
