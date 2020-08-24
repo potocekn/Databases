ca97e42192656b2ea621e9d9815a6fd69a151d51
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-24 15:53:52 +0200

    added method for reading pages from local database

--- Databases/Program.cs
+++ Databases/Program.cs
@@ -9,22 +9,24 @@ namespace Databases
 {
     class Program
     {
-        public string[] ReadLocalDBConfigFile(string path)
+        public LocalDBConfigInfo ReadLocalDBConfigFile(string path)
         {
             return null;
         }
 
-        public static void Update(string[] args)
+        public static List<LocalDBPage> ReadLocalPages()
         {
-            string dataSource = "127.0.0.1";
-            string port = "3306";
-            string userName = "root";
-            string passwd = "password";
-            string databaseName = "local_page_database";
-            string connectionString = String.Format("datasource={0};port={1};username={2};password={3};database={4};",dataSource,port,userName,passwd, databaseName);
-            // Your query,
-            string tableName = "local_pages";
-            string query = String.Format("SELECT * FROM {0}", tableName);
+            LocalDBConfigInfo dbInfo = new LocalDBConfigInfo();
+            dbInfo.DataSource = "127.0.0.1";
+            dbInfo.Port = 3306;
+            dbInfo.UserName = "root";
+            dbInfo.Password = "password";
+            dbInfo.DatabaseName = "local_page_database";
+            dbInfo.TableName = "local_pages";
+
+            string connectionString = dbInfo.GetConnectionString();
+            // Your query,            
+            string query = String.Format("SELECT * FROM {0}", dbInfo.TableName);
 
             // Prepare the connection
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
@@ -33,7 +35,6 @@ namespace Databases
             MySqlDataReader reader;
 
             List<LocalDBPage> localDb = new List<LocalDBPage>();
-
             // Let's do it !
             try
             {
@@ -42,33 +43,27 @@ namespace Databases
 
                 // Execute the query
                 reader = commandDatabase.ExecuteReader();
-
-                // All succesfully executed, now do something
-
-                // IMPORTANT : 
-                // If your query returns result, use the following processor :
-
+                                
                 if (reader.HasRows)
                 {
                     while (reader.Read())
-                    {
-                        // As our database, the array will contain : ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
-                        // Do something with every received database ROW
+                    {                        
                         int id = Convert.ToInt32(reader["page_id"]);
                         string title = reader["page_title"].ToString();
                         string hash = reader["page_hash"].ToString();
                         byte[] content = (byte[])reader["page_content"];
-                        localDb.Add(new LocalDBPage(id, title, hash, content));                       
+                        localDb.Add(new LocalDBPage(id, title, hash, content));
                     }
                 }
                 else
                 {
                     Console.WriteLine("No rows found.");
+                    return null;
                 }
 
                 foreach (LocalDBPage page in localDb)
                 {
-                    Console.WriteLine(String.Format(" Id: {0}\n Title: {1}\n Hash: {2}",page.PageId, page.PageTitle, page.PageHash));
+                    Console.WriteLine(String.Format(" Id: {0}\n Title: {1}\n Hash: {2}", page.PageId, page.PageTitle, page.PageHash));
                     Console.WriteLine("=======================================================================");
                     Console.WriteLine(ASCIIEncoding.ASCII.GetChars(page.PageContent));
                     Console.WriteLine("=======================================================================");
@@ -76,11 +71,25 @@ namespace Databases
                 }
                 // Finally close the connection
                 databaseConnection.Close();
+                return localDb;
             }
             catch (Exception ex)
             {
                 // Show any error message.
                 Console.WriteLine(ex.Message);
+                return null;
+                
+            }
+        }
+
+        public static void Update(string[] args)
+        {
+            List<LocalDBPage> localDBPages = ReadLocalPages();
+            if (localDBPages == null)
+            {
+                Console.WriteLine("Local database for pages is null, nothing to update ...");
+                Console.WriteLine("Ending program ...");
+                return;
             }
         }
         static void Main(string[] args)
