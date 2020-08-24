f8fa8ad1ce3a5dfb26096055613f5832cc2eecaa
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-24 22:08:42 +0200

    added class for important data from mw_text table

---
+++ Databases/MwTextData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databases
+{
+    class MwTextData
+    {
+        public int Id { get; set; }
+        public byte[] Text { get; set; }
+    }
+}
