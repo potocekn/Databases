94e60b6beeb230399046015942237f83dc14d1b4
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-24 22:06:14 +0200

    added class for important data from mw_page table

---
+++ Databases/MwPageData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databases
+{
+    class MwPageData
+    {
+        public string PageId { get; set; }
+        public string Pagetitle { get; set; }
+        public int PageLatest { get; set; }
+    }
+}
