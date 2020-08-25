3b10de6ccc077c9de0dac8d504cc4d93d1eecdf3
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-25 13:56:48 +0200

    created method for reading important info from mw_text table

--- Databases/Program.cs
+++ Databases/Program.cs
@@ -104,6 +104,126 @@ namespace Databases
             return mwInfo;
         }
 
+        static List<MwPageData> ReadMwPageData(string connectionString, string query)
+        {
+            List<MwPageData> mwPageData = new List<MwPageData>();
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["page_id"]);
+                        byte[] title = (byte[])reader["page_title"];
+                        int latest = Convert.ToInt32(reader["page_latest"]);
+                        mwPageData.Add(new MwPageData(id, title, latest));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                    return null;
+                }
+
+                foreach (MwPageData page in mwPageData)
+                {
+                    Console.WriteLine("=======================================================================");
+                    Console.WriteLine(String.Format(" Id: {0}\n Title: {1}\n Latest: {2}", page.PageId, page.GetStringTitleName(), page.PageLatest));
+                    Console.WriteLine("=======================================================================");        
+                }
+                // Finally close the connection
+                databaseConnection.Close();
+                return mwPageData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new MwPageTableConnectionException(ex.Message);
+            }                       
+        }
+
+        static List<MwTextData> ReadMwtextData(string connectionString, string query)
+        {
+            List<MwTextData> mwTextData = new List<MwTextData>();
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["old_id"]);
+                        byte[] content = (byte[])reader["old_text"];
+                        
+                        mwTextData.Add(new MwTextData(id, content));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                    return null;
+                }
+
+                foreach (MwTextData page in mwTextData)
+                {
+                    Console.WriteLine("=======================================================================");
+                    Console.WriteLine(String.Format(" Id: {0}\n Content: {1}", page.Id, ASCIIEncoding.ASCII.GetString(page.Text)));
+                    Console.WriteLine("=======================================================================");
+                }
+                // Finally close the connection
+                databaseConnection.Close();
+                return mwTextData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new MwTextTableConnectionException(ex.Message);
+            }
+        }
+
+        public static List<LocalDBPage> ReadMediawikiPages()
+        {
+            MediawikiDBConfigInfo mwInfo = ReadMediawikiConfigFile("mediawiki.txt");
+
+            string connectionString = mwInfo.GetConnectionString();
+
+            //reading table mw_pages
+            string queryMwPages = String.Format("SELECT * FROM {0}",mwInfo.MwPageTable);
+
+            List<MwPageData> mwPageData = ReadMwPageData(connectionString, queryMwPages);
+
+            string queryMwText = String.Format("SELECT * FROM {0}", mwInfo.MwTextTable);
+
+            List<MwTextData> mwTextData = ReadMwtextData(connectionString, queryMwText);
+
+            return null;
+        }
+
         public static List<LocalDBPage> ReadLocalPages()
         {
             LocalDBConfigInfo dbInfo = ReadLocalDBConfigFile("local_pages.txt");
@@ -171,6 +291,14 @@ namespace Databases
             if (localDBPages == null)
             {
                 Console.WriteLine("Local database for pages is null, nothing to update ...");
+                Console.WriteLine("Ending program ...");
+                return;
+            }
+
+            List<LocalDBPage> mwPageDatas = ReadMediawikiPages();
+            if (mwPageDatas == null)
+            {
+                Console.WriteLine("Mw_page database is null, nothing to update ...");
                 Console.WriteLine("Ending program ...");
                 return;
             }
