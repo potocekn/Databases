83ea42f31825cc6d9ef98d5a3ed19dfcd6776e1b
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-25 13:55:24 +0200

    added method to get string version of Title

--- Databases/MwPageData.cs
+++ Databases/MwPageData.cs
@@ -9,7 +9,23 @@ namespace Databases
     class MwPageData
     {
         public int PageId { get; set; }
-        public string Pagetitle { get; set; }
+        public byte[] PageTitle { get; set; }
         public int PageLatest { get; set; }
+
+        public MwPageData()
+        { 
+        }
+
+        public MwPageData(int id, byte[] title, int latest)
+        {
+            this.PageId = id;
+            this.PageTitle = title;
+            this.PageLatest = latest;
+        }
+
+        public string GetStringTitleName()
+        {
+            return ASCIIEncoding.ASCII.GetString(PageTitle);
+        }
     }
 }
